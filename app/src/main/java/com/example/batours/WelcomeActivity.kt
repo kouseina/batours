3e0cac29f40f540c36b77f076a943209d95f576f
kouseinadaffa <kouseinadaffa@gmail.com>
2023-05-22 23:11:09 +0700

    feat(home): create home with bottom nav

--- app/src/main/java/com/example/batours/WelcomeActivity.kt
+++ app/src/main/java/com/example/batours/WelcomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.batours
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var btnWelcome : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        supportActionBar?.hide()
+
+        btnWelcome = findViewById(R.id.btn_welcome)
+        btnWelcome.setOnClickListener{
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
