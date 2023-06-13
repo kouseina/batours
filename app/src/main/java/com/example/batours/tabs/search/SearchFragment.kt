706057ccdbd4d9173253b2d42c9e0471f77fffb4
kouseina <kouseinadaffa@gmail.com>
2023-06-13 12:47:19 +0700

    fix(dialog): fixing filter chip and dialog

--- app/src/main/java/com/example/batours/tabs/search/SearchFragment.kt
+++ app/src/main/java/com/example/batours/tabs/search/SearchFragment.kt
@@ -1,6 +1,9 @@
 package com.example.batours.tabs.search
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +11,13 @@ import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.batours.R
 import com.example.batours.databinding.FragmentSearchBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class SearchFragment : Fragment() {
 
@@ -46,17 +52,57 @@ class SearchFragment : Fragment() {
 
         btnFilter = view.findViewById(R.id.btn_filer)
 
-        val dialog = Dialog(view.context)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(R.layout.filter_dialog_layout)
-        val btnDialogClose = dialog.findViewById(R.id.btn_close) as TextView
+        showAlertDialog()
+
+    }
+
+    private fun showAlertDialog() {
+        val view = layoutInflater.inflate(R.layout.filter_dialog_layout, null)
+
+        val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
+        var dialog = builder.setView(view).create()
 
         btnFilter.setOnClickListener{view ->
             dialog.show()
         }
 
+        val btnDialogClose: TextView = view.findViewById(R.id.btn_close)
+        val cgRating: ChipGroup = view.findViewById(R.id.cg_rating)
+        val cgCategory: ChipGroup = view.findViewById(R.id.cg_category)
+        val cgPrice: ChipGroup = view.findViewById(R.id.cg_price)
+
         btnDialogClose.setOnClickListener{view ->
             dialog.dismiss()
+        }
+
+        cgRating.setOnCheckedStateChangeListener { group, checkedIds ->
+            if(checkedIds.isNotEmpty()) {
+                val cgItem = group.getChildAt(checkedIds.first() - 1) as Chip
+
+                cgItem.setOnCheckedChangeListener { compoundButton, b ->
+                    if (b) cgItem.setChipStrokeColorResource(R.color.blue) else cgItem.setChipStrokeColorResource(R.color.grey)
+                }
+            }
+        }
+
+        cgCategory.setOnCheckedStateChangeListener { group, checkedIds ->
+            if(checkedIds.isNotEmpty()) {
+                val cgItem = group.getChildAt(checkedIds.first() - 1 - cgRating.childCount) as Chip
+
+                cgItem.setOnCheckedChangeListener { compoundButton, b ->
+                    if (b) cgItem.setChipStrokeColorResource(R.color.blue) else cgItem.setChipStrokeColorResource(R.color.grey)
+                }
+            }
+        }
+
+        cgPrice.setOnCheckedStateChangeListener { group, checkedIds ->
+            if(checkedIds.isNotEmpty()) {
+                val cgItem = group.getChildAt(checkedIds.first() - 1 - cgRating.childCount - cgCategory.childCount) as Chip
+
+                cgItem.setOnCheckedChangeListener { compoundButton, b ->
+                    if (b) cgItem.setChipStrokeColorResource(R.color.blue) else cgItem.setChipStrokeColorResource(R.color.grey)
+                }
+            }
         }
     }
 }
